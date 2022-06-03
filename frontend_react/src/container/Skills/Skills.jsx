584a5ca6baa630a846657116828376662d79db69
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-06-03 11:48:56 +0300

    finished skils component start testimonial

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import "./Skills.scss";
 import { useState, useEffect } from "react";
 import ReactTooltip from "react-tooltip";
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 const Skills = () => {
@@ -83,4 +83,9 @@ const Skills = () => {
   );
 };
 
-export default AppWrap(Skills,'skills');
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
+);
+
